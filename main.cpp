c5f60d5143d81472875f36312a1e163896d3bc37
Ronald Ogando Negron <ronaldogando150@gmail.com>
2020-03-01 15:02:30 -0400

    refactor

--- main.cpp
+++ main.cpp
@@ -1,55 +1,46 @@
 #include <iostream>
 
+#define N 9 
+
 /*
  * @desc Check if a number can be in a certain position [row, column]
  * @param row, column, number, sudoku_board- the sudoku to be solve
  * @return bool - true if find a successful number for the position otherwise return false
  */
-bool check_sudoku(int row,int column,int number,int matrix_board[9][9]){
-	bool notPossible=false;
+bool checkSudoku(int row,int column,int number,int matrix_board[N][N]){
 	
-	for(int i=0;i<9;i++){//Check the row
-		if(matrix_board[row][i]==number){
-			notPossible=true;
-		}			
+	for(int i=0;i<N;i++){//Check the row
+		if(matrix_board[row][i]==number) return false;			
 	}
 	
-	for(int i=0;i<9;i++){//Check the column
-		if(matrix_board[i][column]==number){
-			notPossible=true;
-		}
+	for(int i=0;i<N;i++){//Check the column
+		if(matrix_board[i][column]==number) return false;
 	}
 	
 	row -= row%3;
 	column -= column%3;
 	
-	for(int i=0;i<3;i++){//Check the number in its 3x3 box 
+	for(int i=0;i<3;i++){//Check the square
 		for(int j=0;j<3;j++){
-			if(matrix_board[row+i][column+j]==number){
-				notPossible=true;
-			}
+			if(matrix_board[row+i][column+j]==number) return false;		
 		}
-	}	
-	
-	if(notPossible){
-		return false;
-	}else{
-		return true;
 	}
-
+			
+	return true;	
 }
+
 
 /*
  * @desc Solve a sudoku 9x9
  * @param int matrix[9][9] - the sudoku to be solve
  * @return bool - true if find a successful number for the position otherwise return false
  */
-bool sudoku_solver(int matrix[9][9]){
+bool solveSudoku (int matrix[N][N]){
 	int row, column;
 	bool finished = true;
 	
-	for(int i=0;i<9;i++){
-		for(int j=0;j<9;j++){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
 			if(matrix[i][j]==0){
 				finished = false;
 				row=i;
@@ -61,8 +52,8 @@ bool sudoku_solver(int matrix[9][9]){
 	
 	if(finished){
 		std::cout<<("Solution: \n")<<std::endl;
-		for(int i=0;i<9;i++){
-			for(int j=0;j<9;j++){
+		for(int i=0;i<N;i++){
+			for(int j=0;j<N;j++){
 				std::cout<<matrix[i][j]<<" ";
 			}
 			std::cout<<std::endl;
@@ -71,11 +62,9 @@ bool sudoku_solver(int matrix[9][9]){
 	}
 		
 	for(int i=0;i<10;i++){
-		if(check_sudoku(row,column,i,matrix)){
+		if(checkSudoku(row,column,i,matrix)){
 			matrix[row][column]=i;
-			if(sudoku_solver(matrix)){
-				return true;
-			}
+			if(solveSudoku(matrix)) return true;		
 			matrix[row][column]=0;
 		}
 	}
@@ -86,7 +75,7 @@ bool sudoku_solver(int matrix[9][9]){
 
 int main(){
 
-	int sudokuTable[9][9] ={ 
+	int sudokuTable[N][N] ={ 
 	{5, 3, 0, 0, 7, 0, 0, 0, 0},  
 	{6, 0, 0, 1, 9, 5, 0, 0, 0},  
 	{0, 9, 8, 0, 0, 0, 0, 6, 0},  
@@ -97,12 +86,11 @@ int main(){
 	{0, 0, 0, 4, 1, 9, 0, 0, 5},  
 	{0, 0, 0, 0, 8, 0, 0, 7, 9}}; 
 	                    
-	if(sudoku_solver(sudokuTable)){
+	if(solveSudoku(sudokuTable))
 		std::cout<<"\nSuccesful ;)"<<std::endl;
-	}else{
-		std::cout<<"\nThis suoku cannot be solved, sorry :("<<std::endl;
-	}
-
+	else
+		std::cout<<"\nThis sudoku cannot be solved, sorry :("<<std::endl;
+	
 	return 0;
 }
 
